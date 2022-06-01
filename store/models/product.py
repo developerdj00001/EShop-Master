1bea04a667ba77b967c25d261d764be7a9cee695
DeveloperDj <developerdj00001@outlook.com>
2022-06-01 12:57:33 +0000

    Commit15

--- store/models/product.py
+++ store/models/product.py
@@ -5,4 +5,4 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=200, default='')
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='uploads/products/')
